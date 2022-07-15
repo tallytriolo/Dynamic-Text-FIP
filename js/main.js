ac97d55e28dfcd2bbf1cf908422c66ff86d29451
Tallyta Triolo <tally.soares@gmail.com>
2022-07-15 13:23:32 -0400

    dynamicTxt/FIP

--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
 (() => {
   //variables / DOM connections go here
-  let
-    sigils = document.querySelectorAll('.sigilContainer'),
+let sigils = document.querySelectorAll('.sigilContainer'),
     houseVid = document.querySelector('video'),
     lightBox = document.querySelector ('.lightbox'),
     closeBut = lightBox.querySelector('.close');
 
-    function openLightbox() {
+
+    // variables to change the content when clicked on the shield
+    houseName = document.querySelector('.house-name'),
+    houseInfo = document.querySelector('.house-desc');
+
+const houseData = [
+    //`stark.jpg`, //index 0 // add data-offset ="0" in theimagem into html
+    [`stark`, `stark house info`], //index 0 => houseData[0][0], houseData[0][1]
+    [`baratheon`, `baratheon house info`], //index 1
+    [`lannister`, `lannister house info`], //index 2
+    [`tully`, `tully house info`], //index 3
+    [`greyjoy`, `greyjoy house info`], //index 4
+    [`arryn`, `arryn house info`] //index 5
+];
+
+function showHouseData() {
+    let currentHouse = this.dataset.offset;
+    //debugger;
+    // show the right house info on a sigil click
+    //this refers to the shield you clicked on
+    //dataset.offset is the dataa-offset property defined on thaat html tag
+
+    //`House ${} this is for add the word "House" before the anothers words
+    houseName.textContent = `House ${houseData[this.dataset.offset][0]}`; 
+    houseInfo.textContent = houseData[this.dataset.offset][1];
+    //houseName.src = 'images/houseData [0]; for image
+}
+
+//this function is for show the lightbox
+function openLightbox() {
     lightBox.classList.add('show-lightbox');
     houseVid.currentTime = 0;
     houseVid.play();
@@ -19,7 +47,9 @@ function closeLightbox() {
 }
 
     //event handling here (how is the user going to interact with)
-    sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
+    //sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
+
+    sigils.forEach(sigil => sigil.addEventListener('click', showHouseData));
     
     houseVid.addEventListener('ended', closeLightbox);
     closeBut.addEventListener('click', closeLightbox);
